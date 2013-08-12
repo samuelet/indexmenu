3fb543e5ec38a72e797982debedc86e54397381c
QoStik <gene.umb@gmail.com>
2013-08-12 15:20:25 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author QoStik <gene.umb@gmail.com>
+ */
+$lang['menu']                  = 'Utilitaires Indexmenu';
+$lang['install']               = 'Installer';
+$lang['delete']                = 'Supprimer';
+$lang['check']                 = 'Vérifier';
+$lang['no_repos']              = 'Aucun url de dépot de théme configuré.
+';
+$lang['disabled']              = 'Désactiver';
+$lang['conn_err']              = 'Erreur de connexion';
+$lang['dir_err']               = 'Impossible de créer un répertoire temporaire pour charger le theme';
+$lang['down_err']              = 'Impossible de charger le thme';
+$lang['zip_err']               = 'Erreur de création ou extration ZIP';
+$lang['install_ok']            = 'Theme installé avec succés. Le nouveau theme est disponible dans la bar d\'outils sur la page d\'édition ou avec le <code>js#nom_du_theme option</code>.';
+$lang['install_no']            = 'Erreur de connexion. Cependant vous pouvez essayer de uploader votre theme depuis <a href="http://samuele.netsons.org/dokuwiki/lib/plugins/indexmenu/upload/">ici</a>.';
+$lang['delete_ok']             = 'Theme supprimé avec succés.';
+$lang['delete_no']             = 'Une erreur est survenue pendant la suppresion du theme';
+$lang['upload']                = 'Partage';
+$lang['checkupdates']          = 'Mises à jours Plugin';
+$lang['noupdates']             = 'Indexmenu n\'a pas besoin d\'être mis à jour. Vous avez déjà la derniere version:';
+$lang['js']['indexmenuwizard'] = 'Indexmenu Assistant';
+$lang['js']['index']           = 'Index';
+$lang['js']['options']         = 'Options';
+$lang['js']['navigation']      = 'Navigation';
+$lang['js']['sort']            = 'Trier';
+$lang['js']['filter']          = 'filtrer';
+$lang['js']['performance']     = 'Performance';
+$lang['js']['theme']           = 'Theme';
+$lang['js']['tsort']           = 'Par titre';
+$lang['js']['dsort']           = 'Par date';
+$lang['js']['msort']           = 'Par meta tag';
+$lang['js']['nsort']           = 'Trier aussi les namespaces';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author QoStik <gene.umb@gmail.com>
+ */
+$lang['themes_url']            = 'Télécharger des thémes js depuis cet url http';

9f3f0816ce82a1a65eb76dbc4e98b27084666238
Hideaki SAWADA <chuno@live.jp>
2015-07-11 17:50:33 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['menu']                  = 'Indexmenu 管理';
+$lang['fetch']                 = '表示';
+$lang['install']               = 'インストール';
+$lang['delete']                = '削除';
+$lang['check']                 = '確認';
+$lang['no_repos']              = 'テーマリポジトリのURLが未設定です。';
+$lang['disabled']              = '無効';
+$lang['conn_err']              = '接続エラー';
+$lang['dir_err']               = 'テーマを取得するための一時フォルダが作成できません。';
+$lang['down_err']              = 'テーマを取得できません。';
+$lang['zip_err']               = 'ZIP の作成・展開エラー';
+$lang['install_ok']            = 'テーマは正常にインストールされました。The new theme is avaiable in the toolbar of the edit page or with the <code>js#theme_name option</code>.';
+$lang['install_no']            = '接続エラーが発生しました。しかし、<a href="http://samuele.netsons.org/dokuwiki/lib/plugins/indexmenu/upload/">ここからテーマを手動でアップロードできます</a>。';
+$lang['delete_ok']             = 'テーマは正常に削除されました。';
+$lang['delete_no']             = 'テーマの削除中にエラーが発生しました。';
+$lang['upload']                = '共有';
+$lang['js']['index']           = 'Index';
+$lang['js']['options']         = 'オプション';
+$lang['js']['sort']            = 'ソート';
+$lang['js']['namespace']       = '名前空間';
+$lang['js']['theme']           = 'テーマ';
+$lang['js']['notoc']           = 'TOC プレビュー機能を無効';
+$lang['js']['tsort']           = '見出し順';
+$lang['js']['dsort']           = '日付順';
+$lang['js']['msort']           = 'メタタグ順';
+$lang['js']['nsort']           = '名前空間もソート';
+$lang['js']['rsort']           = 'ページのソートを逆順にする';
+$lang['js']['nons']            = 'ページのみソート';
+$lang['js']['nopg']            = '名前空間のみソート';
+$lang['js']['insert']          = 'indexmenu の追加';
---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['checkupdate']           = '更新を定期的に確認';
+$lang['themes_url']            = 'この HTTP URL から JS テーマをダウンロードしてください。';

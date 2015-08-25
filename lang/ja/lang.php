7eec802533a898c247fb3af4135883d1134eac6b
Hideaki SAWADA <chuno@live.jp>
2015-08-25 13:26:17 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Nobuyuki Fukuyori <self@spumoni.org>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['menu']                  = 'ツール';
 $lang['fetch']                 = '表示';
@@ -17,11 +18,13 @@ $lang['dir_err']               = 'テーマを受取るための一時フォル�
 $lang['down_err']              = 'テーマを受取ることができません';
 $lang['zip_err']               = '圧縮または解凍エラー';
 $lang['install_ok']            = 'テーマのインストールが完了しました。新しいテーマは、編集ページのツールバーからか、<code>js#theme_name option</code>と記述することで使用できます。';
+$lang['install_no']            = '接続エラー。しかし<a href="http://samuele.netsons.org/dokuwiki/lib/plugins/indexmenu/upload/">ここ</a>から自分のテーマを手動でアップロードできます。';
 $lang['delete_ok']             = 'テーマの削除が完了しました。';
 $lang['delete_no']             = 'テーマの削除でエラーが発生しました。';
 $lang['upload']                = '共有';
 $lang['checkupdates']          = 'プラグインの更新';
 $lang['noupdates']             = 'Indexmenuをアップデートする必要はありません。既に最新の状態です。';
+$lang['infos']                 = '<a href="https://www.dokuwiki.org/plugin:indexmenu#theme_tutorial">テーマ学習</a>ページの指示に従って自分のテーマを作成できます。<br />その後、テーマの下の「共有」ボタンで、公共 indexmenu リポジトリに送ってより多くの人を幸せにできます。';
 $lang['showsort']              = 'Indexmenuソート番号';
 $lang['js']['indexmenuwizard'] = 'Indexmenuウィザード';
 $lang['js']['index']           = 'インデックス';
@@ -34,7 +37,33 @@ $lang['js']['namespace']       = '名前空間';
 $lang['js']['nsdepth']         = '深度';
 $lang['js']['js']              = 'レンダリングツリーは、Javasriptによって独自のテーマを定義することができます';
 $lang['js']['theme']           = 'テーマ';
+$lang['js']['navbar']          = '現在の名前空間でツリーを開きます。';
+$lang['js']['nocookie']        = 'ユーザー操作結果のノードの開閉を記憶しません。';
+$lang['js']['noscroll']        = 'コンテナの幅に合わない場合、ツリーをスクロールしないようにします。';
+$lang['js']['notoc']           = '目次プレビュー機能を無効化します。';
 $lang['js']['tsort']           = 'タイトル順';
 $lang['js']['dsort']           = '日付順';
 $lang['js']['msort']           = 'メタ・タグ順';
+$lang['js']['nsort']           = '名前空間もソートする';
+$lang['js']['rsort']           = 'ページソートを逆順にする';
+$lang['js']['nons']            = 'ページのみ表示';
+$lang['js']['nopg']            = '名前空間のみ表示';
 $lang['js']['insert']          = 'Indexmenuの挿入';
+$lang['js']['page']            = 'ページ';
+$lang['js']['revs']            = '履歴';
+$lang['js']['tocpreview']      = '目次プレビュー';
+$lang['js']['editmode']        = '編集モード';
+$lang['js']['ns']              = '名前空間';
+$lang['js']['search']          = '検索中…';
+$lang['js']['searchtooltip']   = 'この名前空間内のページを検索する。';
+$lang['js']['create']          = '作成';
+$lang['js']['headpage']        = 'ヘッドページ';
+$lang['js']['startpage']       = 'スタートページ';
+$lang['js']['startpagetooltip'] = 'このページの下に新規スタートページを作成する';
+$lang['js']['custompagetooltip'] = 'このページの下に新規ページを作成する';
+$lang['js']['exporthtml']      = 'HTML で出力する';
+$lang['js']['exporttext']      = 'テキストで出力する';
+$lang['js']['headpageheretooltip'] = 'この名前空間内に新規ヘッドページを作成する';
+$lang['js']['newpage']         = '新規ページ';
+$lang['js']['newpagetooltip']  = 'この名前空間内に新規ページを作成する';
+$lang['js']['edit']            = '編集';

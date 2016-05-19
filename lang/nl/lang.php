595977880515c7c58f992b1b6f56dba3b14e35e2
Joachim David <joa_david@hotmail.com>
2016-05-19 13:10:05 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author Joachim David <joa_david@hotmail.com>
  */
 $lang['menu']                  = 'Indexmenu Themabeheerder';
 $lang['fetch']                 = 'Weergeven';
@@ -13,17 +14,17 @@ $lang['check']                 = 'Controleer';
 $lang['no_repos']              = 'Geen thema-repositorie url ingesteld';
 $lang['disabled']              = 'Uitgeschakeld';
 $lang['conn_err']              = 'Verbindingsfout';
-$lang['dir_err']               = 'Niet in staat een tijdelijke map te maken om een thema te downloaden';
-$lang['down_err']              = 'Niet in staat een thema te ontvangen';
+$lang['dir_err']               = 'Niet in staat een tijdelijke map te maken om thema te downloaden';
+$lang['down_err']              = 'Niet in staat thema te ontvangen';
 $lang['zip_err']               = 'Zip inpak- of uitpakfout';
 $lang['install_ok']            = 'thema succesvol geïnstalleerd. Het nieuwe thema is beschikbaar in de werkbalk van het bewerkvenster of met <code>js#theme_name</code> syntax';
 $lang['install_no']            = 'Verbindingsfout. Je kunt ook proberen om handmatig je thema te downloaden van <a href="http://samuele.netsons.org/dokuwiki/lib/plugins/indexmenu/upload/">de site van de ontwikkelaar</a>.';
-$lang['delete_ok']             = 'Het thema is succesvol verwijderd.';
-$lang['delete_no']             = 'Er is iets fout gegaan bij verwijderen van het thema';
+$lang['delete_ok']             = 'Thema is succesvol verwijderd.';
+$lang['delete_no']             = 'Er is een fout gebeurd bij verwijderen thema';
 $lang['upload']                = 'Delen';
 $lang['checkupdates']          = 'Plugin updates';
 $lang['noupdates']             = 'Indexmenu heeft geen update nodig. Je hebt al de laatste versie:';
-$lang['infos']                 = 'Je kunt je eigen thema maken met de instructies op de  <a href="https://www.dokuwiki.org/plugin:indexmenu#theme_tutorial">Thema handleiding</a> op de plugin wikipagina. <br /> Je kunt ook meer mensen blijmaken door het thema te delen. Stuur het dan naar de publieke indexmenu repositorie met de "Delen" knop onder dat thema.';
+$lang['infos']                 = 'Je kunt je eigen thema maken met de instructies op de  <a href="https://www.dokuwiki.org/plugin:indexmenu#theme_tutorial">Thema handleiding</a> op de plugin wikipagina. <br /> Je kunt dan meer mensen blij maken :-) Stuur het naar de publieke indexmenu repositorie met de "Delen" knop onder dat thema.';
 $lang['showsort']              = 'Indexmenu sorteernummer:';
 $lang['donation_text']         = 'De indexmenu plugin wordt door niemand gesponsord, maar ik ontwikkel en ondersteun het in mijn vrije tijd. Je kunt mij bedanken of ondersteunen via een donatie.';
 $lang['js']['indexmenuwizard'] = 'Indexmenu Wizard';
@@ -44,15 +45,45 @@ $lang['js']['noscroll']        = 'Voorkom scrollen van de boom als deze niet in 
 $lang['js']['notoc']           = 'Schakel de inhoudsopgave-voorbeeldfunctie uit';
 $lang['js']['tsort']           = 'Op titel';
 $lang['js']['dsort']           = 'Op datum';
-$lang['js']['msort']           = 'Op een metatag';
+$lang['js']['msort']           = 'Op metatag';
 $lang['js']['nsort']           = 'Sorteer ook de namespaces';
 $lang['js']['hsort']           = 'Sorteer startpagina\'s bovenaan';
 $lang['js']['rsort']           = 'Keer de sorteervolgorde van pagina\'s om';
 $lang['js']['nons']            = 'Geef alleen pagina\'s weer';
 $lang['js']['nopg']            = 'Geef alleen namespaces weer';
-$lang['js']['max']             = 'Hoeveel niveau\'s moeten er weergegeven worden met ajax als een knooppunt is geopend? Daarnaast is het aantal subniveau\'s onder dat niveau dat later met AJAX wordt ingeladen, in plaats van meteen bij weergeven van de pagina.';
+$lang['js']['max']             = 'Hoeveel niveau\'s er weergegeven worden met AJAX als een knooppunt is geopend. Daarnaast is het aantal subniveau\'s onder dat niveau dat later met AJAX wordt ingeladen, in plaats van meteen bij weergeven van de pagina.';
 $lang['js']['maxjs']           = 'Hoeveel niveau\'s worden er weergegeven in de browser als een knooppunt wordt geopend';
 $lang['js']['id']              = 'Zelf een cookie-id opgeven voor deze indexmenu';
 $lang['js']['insert']          = 'Indexmenu Invoegen';
 $lang['js']['metanum']         = 'Metadata: sorteernummer';
 $lang['js']['insertmetanum']   = 'Sorteernummer Invoegen';
+$lang['js']['page']            = 'Pagina';
+$lang['js']['revs']            = 'Revisies';
+$lang['js']['tocpreview']      = 'TOC voorbeeld';
+$lang['js']['editmode']        = 'Bewerkingsmodus';
+$lang['js']['insertdwlink']    = 'Invoegen als DWlink';
+$lang['js']['insertdwlinktooltip'] = 'Voeg de link naar deze pagina in in het bewerkingsveld op de positie van de cursor';
+$lang['js']['ns']              = 'Namespace';
+$lang['js']['search']          = 'Zoeken ...';
+$lang['js']['searchtooltip']   = 'Zoeken naar pagina\'s binnen deze namespace';
+$lang['js']['create']          = 'Aanmaken';
+$lang['js']['more']            = 'Meer';
+$lang['js']['headpage']        = 'Hoofdpagina';
+$lang['js']['headpagetooltip'] = 'Maak een nieuwe hoofdpagina onder deze pagina';
+$lang['js']['startpage']       = 'Startpagina';
+$lang['js']['startpagetooltip'] = 'Maak een nieuwe startpagina onder deze pagina';
+$lang['js']['custompage']      = 'Gewone pagina';
+$lang['js']['custompagetooltip'] = 'Maak een nieuwe pagina onder deze pagina';
+$lang['js']['acls']            = 'ACL\'s';
+$lang['js']['purgecache']      = 'Wis cache';
+$lang['js']['exporthtml']      = 'Exporteer als HTML';
+$lang['js']['exporttext']      = 'Exporteer als tekst';
+$lang['js']['headpagehere']    = 'Hoofdpagina hier';
+$lang['js']['headpageheretooltip'] = 'Maak een nieuwe hoofdpagina binnen deze namespace';
+$lang['js']['newpage']         = 'Nieuwe pagina';
+$lang['js']['newpagetooltip']  = 'Maak een nieuwe pagina binnen deze namespace';
+$lang['js']['newpagehere']     = 'Nieuwe pagina hier';
+$lang['js']['insertkeywords']  = 'Voeg keywords in om naar te zoeken binnen deze namespace';
+$lang['js']['insertpagename']  = 'Voeg de paginanaam om aan te maken in';
+$lang['js']['edit']            = 'Bewerken';
+$lang['js']['loading']         = 'Laden...';

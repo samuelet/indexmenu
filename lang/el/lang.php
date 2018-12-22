aa3dfcbd589b067bb3efdf9822f1f4879fe72146
Katerina Katapodi <katerinakatapodi@gmail.com>
2018-12-22 16:15:14 +0100

    translation update

---
+++ lang/el/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <katerinakatapodi@gmail.com>
+ */
+$lang['menu']                  = 'Ιδιότητες του Indexmenu';
+$lang['fetch']                 = 'Προβολή';
+$lang['install']               = 'Εγκατάσταση';
+$lang['delete']                = 'Απαλοιφή';
+$lang['check']                 = 'Έλεγχος ';
+$lang['no_repos']              = 'Δεν έχει δημιουργηθεί αρχείο url';
+$lang['disabled']              = 'Απενεργοποιήθηκε';
+$lang['conn_err']              = 'Σφάλμα κατά την σύνδεση';
+$lang['dir_err']               = 'Δεν μπόρεσε να δημιουργήσει προσωρινό φάκελλο για να λάβει θέμα';
+$lang['down_err']              = 'Δεν μπόρεσε να λάβει θέμα';
+$lang['zip_err']               = 'Δημιουργία zip ή σφάλμα κατά την εξαγωγή';
+$lang['install_ok']            = 'το θέμα δημιουργήθηκε με επιτυχία. Το νέο θέμα είναι διαθέσιμο στην γραμμή εργαλείων της σελίδας έκδοσης ή με το <code>jstheme_name option</code>.';
+$lang['install_no']            = 'Σφάλμα κατά την σύνδεση. Ωστόσο μπορείτε να προσπαθήσετε να ανεβάσετε χειροκίνητα το θέμα σας από <ένα href="http://samuele.netsons.org/dokuwiki/lib/plugins/indexmenu/upload/">εδώ</a>.	';
+$lang['delete_ok']             = 'Το θέμα διαγράφηκε με επιτυχία';
+$lang['delete_no']             = 'Συνέβη σφάλμα κατά την διάρκεια της απαλοιφής θέματος';
+$lang['upload']                = 'Μοιραστείτε';
+$lang['checkupdates']          = 'Ενημερώσεις plugin';
+$lang['noupdates']             = 'To Ιndexmenu δεν χρειάζεται να ενημερωθεί. Έχετε ήδη την τελευταία έκδοση:';
+$lang['infos']                 = 'Μπορείτε να δημιουργήσετε το θέμα σας ακολουθώντας τις οδηγίες στο  <a href="https://www.dokuwiki.org/plugin:indexmenu#theme_tutorial">Theme Tutorial</a> page. <br />Τότε μπορείτε να δώσετε χαρά στο κοινό  :-) αποθηκεύοντας τον στη θέση indexmenu , με το κουμπί  "αποστολή " κάτω από αυτό το θέμα.';
+$lang['showsort']              = 'Αριθμός σειράς Indexmenu:';
+$lang['donation_text']         = 'Η εγγραφή indexmenu δεν σπονσοράρεται από κανέναν αλλά εγώ δημιουργώ και το υποστηρίζω στον ελεύθερο χρόνο μου. Αν αποκτήσετε κάτι χάριν σε αυτό ή θέλετε να συμβάλλετε στην δημιουργία του, κάνετε μια δωρεά.';
+$lang['js']['indexmenuwizard'] = 'Indexmenu Wizard	';
+$lang['js']['index']           = 'Κατάλογος';
+$lang['js']['options']         = 'Επιλογές';
+$lang['js']['navigation']      = 'Πλοήγηση';
+$lang['js']['sort']            = 'Σειρά';
+$lang['js']['filter']          = 'Φίλτρο';
+$lang['js']['performance']     = 'Απόδοση';
+$lang['js']['namespace']       = 'Κενός χώρος για όνομα';
+$lang['js']['nsdepth']         = 'Βάθος';

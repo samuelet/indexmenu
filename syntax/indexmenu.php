1e8caaabd824b713fb9deec6bf5323cd477cd66f
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2022-09-14 15:10:36 -0300

    nsort sorts namespaces and pages together (5.0)
    Similar to https://github.com/samuelet/indexmenu/pull/111 but updated for IndexMenu 5.0 release.

--- syntax/indexmenu.php
+++ syntax/indexmenu.php
@@ -870,6 +870,14 @@ class syntax_plugin_indexmenu_indexmenu extends DokuWiki_Syntax_Plugin {
             $files[] = $dir.'/'.$file;
         }
         closedir($dh);
+        
+        //Collect and sort files
+        foreach($files as $file) {
+            call_user_func_array($func, array(&$files_tmp, $base, $file, 'f', $lvl, $opts));
+        }
+        if(!$this->nsort) {
+            usort($files_tmp, array($this, "_cmp"));
+        }
 
         //Collect and sort dirs
         if($this->nsort) {
@@ -877,13 +885,14 @@ class syntax_plugin_indexmenu_indexmenu extends DokuWiki_Syntax_Plugin {
             foreach($dirs as $dir) {
                 call_user_func_array($func, array(&$dirs_tmp, $base, $dir, 'd', $lvl, $opts));
             }
-            //sort directories
-            usort($dirs_tmp, array($this, "_cmp"));
+            //sort directories with pages
+            $dirs_and_files = array_merge($dirs_tmp, $files_tmp);
+            usort($dirs_and_files, array($this, "_cmp"));
             //add and search each directory
-            foreach($dirs_tmp as $dir) {
-                $data[] = $dir;
-                if($dir['return']) {
-                    $this->_search($data, $base, $func, $opts, $dir['file'], $lvl + 1);
+            foreach($dirs_and_files as $dir_or_file) {
+                $data[] = $dir_or_file;
+                if($dir_or_file['type'] != 'f' && $dir_or_file['return']) {
+                    $this->_search($data, $base, $func, $opts, $dir_or_file['file'], $lvl + 1);
                 }
             }
         } else {
@@ -897,12 +906,6 @@ class syntax_plugin_indexmenu_indexmenu extends DokuWiki_Syntax_Plugin {
             }
         }
 
-        //Collect and sort files
-        foreach($files as $file) {
-            call_user_func_array($func, array(&$files_tmp, $base, $file, 'f', $lvl, $opts));
-        }
-        usort($files_tmp, array($this, "_cmp"));
-
         //count added items
         $added = count($data) - $count;
 
@@ -912,7 +915,9 @@ class syntax_plugin_indexmenu_indexmenu extends DokuWiki_Syntax_Plugin {
             if(!$v['hns']) array_pop($data);
         } else {
             //add files to index
-            $data = array_merge($data, $files_tmp);
+            if(!$this->nsort) {
+                $data = array_merge($data, $files_tmp);
+            }
         }
     }
 

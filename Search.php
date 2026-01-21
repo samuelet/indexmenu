ba9039c2294cb2ab38aa5d5f05dd53ebe00195db
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2026-01-21 13:11:20 -0300

    Refactor condition to check for empty 'hns' value

--- Search.php
+++ Search.php
@@ -698,7 +698,7 @@ class Search
         if ($added === 0 && $files_tmp === []) {
             //remove empty directory again, only if it has not a headpage associated
             $lastItem = end($data);
-            if (isset($lastItem['hns']) && !$lastItem['hns']) {
+            if (empty($lastItem['hns'])) {
                 array_pop($data);
             }
         } elseif (!($this->nsort && !$this->group)) {

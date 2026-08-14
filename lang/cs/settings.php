73fd3e1e2610f7cff16813d191e3d21b4e83c84e
Gerrit Uitslag <klapinklapin@gmail.com>
2026-08-15 00:01:21 +0200

    Revert "Translation update (cs)"

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,7 +3,6 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author E32k <Erik32konto@gmail.com>
  * @author Vojta Olsan <olsh0@seznam.cz>
  */
-$lang['checkupdate']           = 'Pravidelně kontrolovat aktualizace.';
+$lang['checkupdate']           = 'Pravidelně kontrolovat aktualizace';
